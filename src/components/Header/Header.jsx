c37fc104a1aa9fe56e8e34fef52f3c020153910f
makhnev-a <ray.makhnev@gmail.com>
2020-04-21 16:32:24 +0300

    header fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,9 +3,27 @@ import styles from './Header.module.css';
 
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <header className={styles.header}>
+            <nav className={styles.headerNavigation}>
+                <a href={'/'} className={styles.headerLogo}>
+                    <img src="" alt="Логотип"/>
+                </a>
+                <ul className={styles.headerLinks}>
+                    <li>
+                        <a href="/">Главная</a>
+                    </li>
+                    <li>
+                        <a href={'/skills'}>Скиллы</a>
+                    </li>
+                    <li>
+                        <a href={'/works'}>Работы</a>
+                    </li>
+                    <li>
+                        <a href={'/contacts'}>Контакты</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     );
 };
 
