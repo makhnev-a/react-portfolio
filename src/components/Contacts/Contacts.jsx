404d2b942ad658808b4d149dbb4e5a85dd9b1dd4
makhnev-a <ray.makhnev@gmail.com>
2020-04-21 17:30:00 +0300

    contacts fix

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -3,9 +3,18 @@ import styles from './Contacts.module.css';
 
 const Contacts = () => {
     return (
-        <div>
-            Contacts block
-        </div>
+        <section className={styles.contacts}>
+            <div className={styles.contactsBlock}>
+                <h2>Контакты</h2>
+                <form className={styles.contactsForm}>
+                    <input type="text"/>
+                    <input type="text"/>
+                    <textarea>
+                    </textarea>
+                </form>
+                <button>Отправить</button>
+            </div>
+        </section>
     );
 };
 
