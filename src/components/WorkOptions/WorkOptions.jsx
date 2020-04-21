1125773036b146a61a8785b240dff9c8863644ae
makhnev-a <ray.makhnev@gmail.com>
2020-04-21 17:25:11 +0300

    work options fix

--- src/components/WorkOptions/WorkOptions.jsx
+++ src/components/WorkOptions/WorkOptions.jsx
@@ -3,9 +3,12 @@ import styles from './WorkOptions.module.css';
 
 const WorkOptions = () => {
     return (
-        <div>
-            Work options block
-        </div>
+        <section className={styles.workOptions}>
+            <div className={styles.workOptionsBlock}>
+                <h2>Рассматриваю варианты удаленной работы</h2>
+                <button>Нанять меня</button>
+            </div>
+        </section>
     );
 };
 
