b3c070dc8a71da8bdd8e37597dcb9a433417d61c
makhnev-a <ray.makhnev@gmail.com>
2020-04-22 23:35:08 +0300

    add grid fix 4

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,14 +4,17 @@ import styles from './Footer.module.css';
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <div className={styles.footerBlock}>
-                <ul className={styles.footerList}>
-                    <li>item</li>
-                    <li>item</li>
-                    <li>item</li>
-                    <li>item</li>
-                </ul>
-                <p className={styles.footerCopy}>© 2019 Все права защищены</p>
+            <div className={'container'}>
+                <div className={styles.footerBlock}>
+                    <p className={styles.footerCopy}>Иван Иванов</p>
+                    <ul className={styles.footerList}>
+                        <li>item</li>
+                        <li>item</li>
+                        <li>item</li>
+                        <li>item</li>
+                    </ul>
+                    <p className={styles.footerCopy}>© 2019 Все права защищены</p>
+                </div>
             </div>
         </footer>
     );
