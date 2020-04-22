e01d2e1ad179d7ed0f796d187293bcf394d05778
makhnev-a <ray.makhnev@gmail.com>
2020-04-22 19:31:20 +0300

    add grid fix 3

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -4,15 +4,19 @@ import styles from './Contacts.module.css';
 const Contacts = () => {
     return (
         <section className={styles.contacts}>
-            <div className={styles.contactsBlock}>
-                <h2>Контакты</h2>
-                <form className={styles.contactsForm}>
-                    <input type="text"/>
-                    <input type="text"/>
-                    <textarea>
-                    </textarea>
-                </form>
-                <button>Отправить</button>
+            <div className={'container'}>
+                <div className={styles.contactsBlock}>
+                    <h2>Контакты</h2>
+                    <form className={styles.contactsForm}>
+                        <input type="text" placeholder={'Первый инпунт'}/>
+                        <br/>
+                        <input type="text" placeholder={'Второй инпут'}/>
+                        <br/>
+                        <textarea placeholder={'Введите в поле текст'}>
+                        </textarea>
+                    </form>
+                    <button>Отправить</button>
+                </div>
             </div>
         </section>
     );
--- src/components/WorkOptions/WorkOptions.jsx
+++ src/components/WorkOptions/WorkOptions.jsx
@@ -4,9 +4,11 @@ import styles from './WorkOptions.module.css';
 const WorkOptions = () => {
     return (
         <section className={styles.workOptions}>
-            <div className={styles.workOptionsBlock}>
-                <h2>Рассматриваю варианты удаленной работы</h2>
-                <button>Нанять меня</button>
+            <div className={'container'}>
+                <div className={styles.workOptionsBlock}>
+                    <h2>Рассматриваю варианты удаленной работы</h2>
+                    <button>Нанять меня</button>
+                </div>
             </div>
         </section>
     );
