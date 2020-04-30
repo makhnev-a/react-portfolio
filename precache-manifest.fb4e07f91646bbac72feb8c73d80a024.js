4b39e815f1d72a097d832c670686f35bd330961a
makhnev-a <ray.makhnev@gmail.com>
2020-04-30 17:11:20 +0300

    Updates

---
+++ precache-manifest.fb4e07f91646bbac72feb8c73d80a024.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dfa67ad8abcec77315424c74972c7c0",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "ef888bbcc28dc26a4193",
+    "url": "/react-portfolio/static/css/main.2ad314f2.chunk.css"
+  },
+  {
+    "revision": "b5b0e2c9460d90684ede",
+    "url": "/react-portfolio/static/js/2.1f9ab809.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.1f9ab809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef888bbcc28dc26a4193",
+    "url": "/react-portfolio/static/js/main.92ee95dd.chunk.js"
+  },
+  {
+    "revision": "0938b211af4f266a20b0",
+    "url": "/react-portfolio/static/js/runtime-main.0319e79d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
