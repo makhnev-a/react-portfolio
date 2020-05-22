267aef7d5f04030883e8d2a35f41913ab9322efa
makhnev-a <ray.makhnev@gmail.com>
2020-05-22 14:42:29 +0300

    add font-awesome icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import styles from './Footer.module.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faFacebook, faInstagram, faTwitter, faVk} from "@fortawesome/free-brands-svg-icons";
+import {faCircle, faSpinner} from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
     return (
@@ -7,10 +10,18 @@ const Footer = () => {
             <div className={styles.footerBlock}>
                 <p className={styles.footerCopy}>Иван Иванов</p>
                 <ul className={styles.footerList}>
-                    <li>item</li>
-                    <li>item</li>
-                    <li>item</li>
-                    <li>item</li>
+                    <li>
+                        <FontAwesomeIcon icon={faVk} size={"3x"} mask={faCircle}/>
+                    </li>
+                    <li>
+                        <FontAwesomeIcon icon={faFacebook} size={"3x"} mask={faCircle}/>
+                    </li>
+                    <li>
+                        <FontAwesomeIcon icon={faInstagram} size={"3x"} mask={faCircle}/>
+                    </li>
+                    <li>
+                        <FontAwesomeIcon icon={faTwitter} size={"3x"} mask={faCircle}/>
+                    </li>
                 </ul>
                 <p className={styles.footerCopy}>© 2019 Все права защищены</p>
             </div>
